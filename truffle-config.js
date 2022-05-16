8696d7326bafe265c5df8a0d11f540b0724fc791
xcellent-dev <xcellentdev@gmail.com>
2022-05-16 10:59:40 +0800

    init
    Signed-off-by: xcellent-dev <xcellentdev@gmail.com>

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  /**
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+    contracts_directory: './src/contracts',
+    contracts_build_directory: './src/abis',
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "^0.8.0",   
+      optimizer: {
+        enabled: 'true',
+        runs: 200
+      }
+    }
+  },
+
+  
+};
